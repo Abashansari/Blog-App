3856574944adb408c707d0bec39a6b92b4e2397e
Abash Ansari <ansariabash2004@gmail.com>
2025-07-27 04:07:03 -0400

    insert/added a logo & linked with router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,26 +4,27 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png'
 
 export default function Navbar() {
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Blog</a>
+                    <img src={logo} alt='logo' width='250px' />
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">About</a>
+                                <Link className="nav-link" to="/about">About Me</Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Lifestyle</a>
+                                <Link className="nav-link" to="/blog">Blog</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to="/write">Write Blogs</Link>
