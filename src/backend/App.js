d398b560e6041b19894fa51f3ef63a8b824e5364
Abash Ansari <ansariabash2004@gmail.com>
2025-07-29 08:08:18 -0400

    created a server

---
+++ src/backend/App.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express();
+const port = 5000;
+
+try {
+    app.get('/', (req, res) => {
+        res.send("Hello from server");
+    })
+    app.listen(port, () => {
+        console.log(`App is listening at http://localhost:${port}`);
+    })
+} catch (error) {
+    console.error('Failed to start server :', error.message());
+}
