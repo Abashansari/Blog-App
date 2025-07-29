00afbdf7e61793defef7ba7871c30c2b198e4f62
Abash Ansari <ansariabash2004@gmail.com>
2025-07-29 02:13:22 -0400

    inserted a card

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import CardItems from './CardItems'
 
 export default function Blogs() {
   return (
+    <>
     <div>This is Blog page</div>
+    <CardItems />
+
+    </>
   )
 }
