22448a42cad81a182b9d312b999729db2f5d5209
Abash Ansari <ansariabash2004@gmail.com>
2025-09-01 12:28:45 -0400

    import cors

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,10 +1,17 @@
 import express from 'express'
+import cors from 'cors'
 import connectToMongo from './db.js';
+import blogRoutes from './routes/blogRoute.js';
 
 const app = express();
 const port = 5000;
 
 connectToMongo()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/addBlog',blogRoutes)
 
 try {
     app.get('/', (req, res) => {
