2e4b8122c6e0725bda3df7ea65ac9a5cc6eca80b
Abash Ansari <ansariabash2004@gmail.com>
2025-09-05 16:21:23 +0530

    removed try-catch statement

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,25 +1,22 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 import connectToMongo from './db.js';
 import blogRoutes from './routes/blogRoute.js';
 
 const app = express();
 const port = 5000;
 
-connectToMongo()
+connectToMongo();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json()); 
 
-app.use('/addBlog',blogRoutes)
+app.use('/api/blog', blogRoutes);
 
-try {
-    app.get('/', (req, res) => {
-        res.send("Hello from server");
-    })
-    app.listen(port, () => {
-        console.log(`App is listening at http://localhost:${port}`);
-    })
-} catch (error) {
-    console.error('Failed to start server :', error.message());
-}
+app.get('/', (req, res) => {
+  res.json({ message: "Server is working!" });
+});
+
+app.listen(port, () => {
+  console.log(`App is listening at http://localhost:${port}`);
+});
