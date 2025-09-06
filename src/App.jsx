fdaf69b3001af74471794d768f0c1121e630bdbf
Abash Ansari <ansariabash2004@gmail.com>
2025-09-06 14:46:20 +0530

    import "BlogApp" component and replace all other components in BlogApp component

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import Form from './components/Form'
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom'
-import Footer from './components/Footer'
-import About from './components/About'
-import Blogs from './components/Blogs'
-import Home from './components/Home'
+import BlogApp from './components/BlogApp'
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/write' element={<Form />} />
-          <Route path='/blog' element={<Blogs/>} />
-        </Routes>
-      </Router>
-    <Footer />
+      <BlogApp />
     </>
   )
 }
