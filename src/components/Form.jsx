ea695a75d0938c2dc6af5e931c8fa7c9a9b8980f
Abash Ansari <ansariabash2004@gmail.com>
2025-09-06 11:35:10 +0530

    update lable's name & added "*" to all label

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,22 +14,18 @@ export default function Form() {
                     </div>
 
                     <div className="mb-3">
-                        <label htmlFor="inputDescription" className="form-label">Short Description</label>
+                        <label htmlFor="inputDescription" className="form-label">Introduction<span className='text-danger'>*</span></label>
+                        <textarea className="form-control" id="inputDescription" rows="3" placeholder="Write a short introduction..."></textarea>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="inputDescription" className="form-label">Summary<span className='text-danger'>*</span></label>
                         <textarea className="form-control" id="inputDescription" rows="3" placeholder="Write a short summary..."></textarea>
                     </div>
 
                     <div className="mb-3">
-                        <label className="form-label d-block">Upload Blog Image</label>
-                        <div className="input-group">
-                            <input type="file" className="form-control" id="inputImage" accept='.jpg, .jpeg, .png' />
-                            <label className="input-group-text" htmlFor="inputImage">Upload</label>
-                        </div>
-                        <small className="form-text text-muted">Accepted formats: JPG, PNG</small>
-                    </div>
-
-                    <div className="mb-3">
-                        <label htmlFor="inputConclusion" className="form-label">Conclusion</label>
-                        <textarea className="form-control" id="inputConclusion" rows="2" placeholder="Final thoughts or summary"></textarea>
+                        <label htmlFor="inputConclusion" className="form-label">Your Thoughts<span className='text-danger'>*</span></label>
+                        <textarea className="form-control" id="inputConclusion" rows="2" placeholder="Final thoughts"></textarea>
                     </div>
 
                     <button type="submit" className="btn btn-primary w-100">Publish Blog</button>
