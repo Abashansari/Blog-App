45b70ed37fe3280539276aa505f4be10b9fa7952
Abash Ansari <ansariabash2004@gmail.com>
2025-09-06 11:05:13 +0530

    added a icon and design the navbar theme

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,30 @@
 import React from 'react'
-import logo from '../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import LaptopChromebookIcon from '@mui/icons-material/LaptopChromebook';
 
 export default function Navbar() {
     return (
-        <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <img src={logo} alt='logo' width='250px' />
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/about">About Me</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/blog">Blog</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/write">Write Blogs</Link>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#contact">Contact</a>
-                            </li>
-                        </ul>
-                    </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm mb-6 fixed-top">
+            <div className="container-fluid">
+
+                <a className="navbar-brand fw-bold text-primary d-flex align-items-center" href="#">
+                    <LaptopChromebookIcon fontSize='large' className='me-2' /> Abash Ansari
+                </a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 fw-semibold">
+                        <li className="nav-item"><Link className="nav-link text-dark" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-dark" to="/about">About Me</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-dark" to="/blog">Blog</Link></li>
+                        <li className="nav-item"><Link className="nav-link text-dark" to="/write">Write Blogs</Link></li>
+                        <li className="nav-item"><a className="nav-link text-dark" href="#contact">Contact</a></li>
+                    </ul>
                 </div>
-            </nav>
-        </>
+            </div>
+        </nav>
     )
 }
