1dfa3e122389cac231e588dda44fb02632b58e51
Abash Ansari <ansariabash2004@gmail.com>
2025-09-07 18:23:29 +0530

    added http link in image's src

--- src/components/CardItems.jsx
+++ src/components/CardItems.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
 import abash from '../assets/abash.jpeg'
 
-export default function CardItems({author,title,introduction,img,summary,thoughts}) {
+export default function CardItems({author,title,introduction,image,summary,thoughts}) {
   return (
     <>
-      {/* <div className="card" style={{ width: "18rem;" }}>
-        <img src={abash} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-     */}
-
-
 <div className="card text-center">
   <div className="card-header">
     {author}
@@ -22,7 +11,7 @@ export default function CardItems({author,title,introduction,img,summary,thought
   <div className="card-body">
     <h5 className="card-title">{title}</h5>
     <p className="card-text">{introduction}</p>
-    {/* <img src={img} class="card-img-center" alt="..."></img> */}
+    <img src={`http://localhost:5000/upload/${image}`} className="card-img-center" ></img>
     <p className="card-text">{summary}</p>
     <p className="card-text">{thoughts}</p>
     <a href="#" className="btn btn-primary">Go somewhere</a>
