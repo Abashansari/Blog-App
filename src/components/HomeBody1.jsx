d5bb36a49a45cd4d98bf91f6bdf233abde95c86d
Abash Ansari <ansariabash2004@gmail.com>
2025-09-07 11:49:08 +0530

    removed card element & import Blog component

--- src/components/HomeBody1.jsx
+++ src/components/HomeBody1.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import abash from '../assets/abash.jpeg'
+import Blogs from './Blogs'
 
 export default function HomeBody1() {
   return (
@@ -10,13 +11,7 @@ export default function HomeBody1() {
             <div className="col-md-4">
               <img src={abash} className="img-fluid rounded-start" alt="..." />
             </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-              </div>
-            </div>
+<Blogs />
           </div>
         </div>
       </div>
