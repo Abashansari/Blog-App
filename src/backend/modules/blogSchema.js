3d8d3947e5df2c6f9dda83af7ec58f22b5aa84f3
Abash Ansari <ansariabash2004@gmail.com>
2025-09-07 15:23:46 +0530

    update Schema by re-adding img field

--- src/backend/modules/blogSchema.js
+++ src/backend/modules/blogSchema.js
@@ -9,21 +9,25 @@ const blogSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    img: {
+        type: String,
+        required: true
+    },
     summary: {
         type: String,
         required: true
     },
     thoughts: {
-        type : String,
-        required:true
+        type: String,
+        required: true
     },
-    author : {
-        type : String,
-        default:"Admin"
+    author: {
+        type: String,
+        default: "Admin"
     },
-    date : {
-        type : Date,
-        default:Date.now
+    date: {
+        type: Date,
+        default: Date.now
     }
 });
 
