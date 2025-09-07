84ea70c2366c6e9d0213357195b1ef6eec30eb3d
Abash Ansari <ansariabash2004@gmail.com>
2025-09-07 11:50:16 +0530

    addded a function to bringt blogs from database

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardItems from './CardItems'
+import axios from 'axios'
 
 export default function Blogs() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+
+    const getBlogs = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/api/blog/getBlog`)
+        setBlogs(response.data)
+        console.log(response.data)
+
+      } catch (error) {
+        console.log("failed to fetch data from backend", error.message)
+      }
+    }
+    getBlogs()
+  }, [])
+
   return (
     <>
-      <div>This is Blog page</div>
-      <CardItems />
+      <div className='container'>This is Blog page</div>
+      {blogs.map((blog) => {
+        return <CardItems key={blog._id} title={blog.title} introduction={blog.introduction} summary={blog.summary} thoughts={blog.thoughts} />
+      })}
     </>
   )
 }
